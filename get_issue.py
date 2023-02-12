cc33e9741f0829865a7b2f7589a017ff2f45b69b
yeonjiLim <view7186@dalcomsoft.com>
2023-02-13 03:56:58 +0900

    레포지토리 단위로 issue, pr get 요청

--- get_issue.py
+++ get_issue.py
@@ -1,10 +1,18 @@
 import datetime
-from github import Organization
+from github import Organization, Repository
+
+last_week = datetime.datetime.now()-datetime.timedelta(weeks=1)
 
 def get_all_issues(o: Organization.Organization):
     print('===== start : get_all_issues =====')
-    return (i for r in o.get_repos() for i in r.get_issues(state="closed") if i.user.name != "Deleted user")
+    return [i for r in o.get_repos() for i in r.get_issues(state="closed") if i.user.name != "Deleted user"]
+
 
 def get_all_issues_in_last_week(o: Organization.Organization):
     print('===== start : get_all_issues_in_last_week =====')
-    return (i for r in o.get_repos() for i in r.get_issues(state="closed") if i.user.name != "Deleted User" and i.closed_at >= (datetime.datetime.now()-datetime.timedelta(weeks=1)))
+    return [i for r in o.get_repos() for i in r.get_issues(state="closed", since=last_week) if i.user.name != "Deleted User" and i.closed_at >= last_week]
+
+
+def get_repo_issues_last_week(r: Repository.Repository):
+    print('===== start : get_repo_issues_last_week =====')
+    return [i for i in r.get_issues(state="closed", since=last_week) if i.user.name != "Deleted User" and i.closed_at >= last_week]
--- get_pr.py
+++ get_pr.py
@@ -1,10 +1,18 @@
 import datetime
-from github import Organization
+from github import Organization, Repository
+
+last_week = datetime.datetime.now()-datetime.timedelta(weeks=1)
 
 def get_all_prs(o: Organization.Organization):
     print('===== start : get_all_prs =====')
-    return (p for r in o.get_repos() for p in r.get_pulls(state="closed") if p.user.name != "Deleted user")
+    return [p for r in o.get_repos() for p in r.get_pulls(state="closed") if p.user.name != "Deleted user"]
+
 
 def get_all_prs_in_last_week(o: Organization.Organization):
     print('===== start : get_all_prs_in_last_week =====')
-    return (p for r in o.get_repos() for p in r.get_pulls(state="closed") if p.user.name != "Deleted User" and p.closed_at >= (datetime.datetime.now()-datetime.timedelta(weeks=1)))
+    return [p for r in o.get_repos() for p in r.get_pulls(state="closed") if p.user.name != "Deleted User" and p.closed_at >= last_week]
+
+
+def get_repo_prs_last_week(r: Repository.Repository):
+    print('===== start : get_repo_prs_last_week =====')
+    return [p for p in r.get_pulls(state="closed") if p.user.name != "Deleted User" and p.closed_at >= last_week]
